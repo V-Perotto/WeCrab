dda4fc4f556900609e36b88ad05b332f52660e40
Vittorio Perotto <vittorio.perotto@outlook.com>
2020-11-18 19:11:31 -0300

    Adicionando barra de pesquisa

---
+++ scripts/js/search.js
@@ -0,0 +1,15 @@
+function procura() {
+
+    color = document.getElementById("search-input").value;
+
+    if (color === "Sunlight Crab") {
+        window.location.href = "../../pages/produtos.html#orange"
+    }
+    else if (color === "Midnight Crab") {
+        window.location.href = "../../pages/produtos.html#purple"
+    }
+    else if (color === "Black Crab") {
+        window.location.href = "../../pages/produtos.html#black"
+    }
+
+} 
